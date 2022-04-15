20d1a63027a529f0237aee35aae8a226593296cf
Katharina Bracher <katharina.bracher@mailbox.org>
2022-04-15 10:36:48 +0200

    testing simulation in new classes

---
+++ code/onetwogo/__init__.py
@@ -0,0 +1,249 @@
+from typing import Dict
+import numpy as np
+from scipy.stats import linregress
+from scipy import signal
+from sklearn.decomposition import PCA
+from .functions import sigmoid
+
+
+class Params:
+    Wuv: float
+    """ Test"""
+
+    Wut: float
+
+    def __init__(
+        self,
+        Wut=6,
+        Wuv=6,
+        Wvt=6,
+        Wvu=6,
+        dt=10,
+        tau=150,
+        sigma=0.01,
+        th=0.65,
+        IF=50,
+        ntrials=100,
+        uinit=0.7,
+        vinit=0.2,
+        yinit=0.5,
+        first_duration=750,
+        delay=500
+    ):
+        self.Wut = Wut
+        self.Wuv = Wuv
+        self.Wvt = Wvt
+        self.Wvu = Wvu
+        self.dt = dt
+        self.tau = tau
+        self.sigma = sigma
+        self.th = th
+        self.IF = IF
+        self.ntrials = ntrials
+        self.uinit = uinit
+        self.vinit = vinit
+        self.yinit = yinit
+        self.first_duration = first_duration
+        self.delay = delay
+
+    def from_dict(PARAMS_DICT: Dict[str, float]):
+        return Params(**PARAMS_DICT)
+
+
+class BaseSimulation:
+
+    def __init__(self, params: Params):
+        self.params = params
+
+    def network(self, state_init, reset, K, nbin):
+        params = self.params
+
+        u, v, y, I = state_init.copy()
+        ntrials = u.shape[0]
+
+        # save bool with resets=1 over time
+        reset_lst = []
+        simulation = np.zeros([nbin, 4, ntrials])
+
+        for i in range(nbin):
+            I += (reset * K * (y - params.th)) / params.tau * params.dt
+            u += (-u + sigmoid(params.Wut * I - params.Wuv * v - params.IF * reset +
+                  np.random.randn(ntrials) * params.sigma)) / params.tau * params.dt
+            v += (-v + sigmoid(params.Wvt * I - params.Wvu * u + params.IF * reset +
+                  np.random.randn(ntrials) * params.sigma)) / params.tau * params.dt
+            y += (-y + u - v + np.random.randn(ntrials)
+                  * params.sigma) / params.tau * params.dt
+
+            simulation[i] = [u.copy(), v.copy(), y.copy(), I.copy()]
+            reset_lst.append(reset)
+
+        return simulation, reset_lst
+
+    def trial_update(self, simulation, reset_lst, reset, K, nbin, production_step=False):
+        # get prev I,v,u,y to continue trial or experiment
+        state_init = simulation[-1]
+        # next step simulation
+        simulation2, reset_lst2 = self.network(state_init, reset, K, nbin)
+
+        # 0.4 of stim duration considered early phase
+        earlyphase = int(0.4*nbin/2)
+
+        if production_step:
+            return self.production_step(
+                simulation, reset_lst, simulation2, reset_lst2, nbin, earlyphase)
+
+        # for all stages exept production
+        simulation = np.concatenate((simulation, simulation2))
+        reset_lst.extend(reset_lst2)
+        return simulation, reset_lst
+
+    def production_step(_simulation, _reset_lst, _simulation2, _reset_lst2, _nbin: int, _earlyphase):
+        pass
+
+    def statistics(self, s, production):
+        std_s = np.std(s, 2)
+        production = np.array(production)*self.dt
+        mean = np.mean(production)
+        std = np.std(production)
+        return mean, std, std_s
+
+    def plot_behavior(self, stimuli_range, K, initI, ax=None):
+        mean_lst = []
+        std_lst = []
+
+        for stim in stimuli_range:
+            simu, res, production, timeout_trials = self.simulate_parallel(stim, K, initI)
+            # print('stimulus', stim, '; timout trials', len(timeout_trials))
+            mean, std, _ = self.statistics(simu, production)
+            mean_lst.append(mean)
+            std_lst.append(std)
+
+        reg = linregress(stimuli_range, mean_lst)
+
+        if ax is None:
+            plt.errorbar(stimuli_range, mean_lst, yerr=std_lst, fmt='-o', c='k')
+            plt.plot([stimuli_range[0]-100, stimuli_range[-1]+100],
+                     [stimuli_range[0]-100, stimuli_range[-1]+100], c='grey', linestyle='--')
+            plt.text(np.min(stimuli_range)-100, np.max(stimuli_range)+100, 'slope='+str(round(reg[0], 3)))
+            plt.xlabel('Stimulus (ms)')
+            plt.ylabel('Production (ms)')
+        else:
+            subplot = ax.errorbar(stimuli_range, mean_lst, yerr=std_lst, fmt='-o', c='k')
+            ax.plot([stimuli_range[0]-100, stimuli_range[-1]+100],
+                    [stimuli_range[0]-100, stimuli_range[-1]+100], c='grey', linestyle='--')
+            ax.text(np.min(stimuli_range)-100, np.max(stimuli_range)+50, 'slope=' +
+                    str(round(reg[0], 3))+', to='+str(len(timeout_trials)))
+            return subplot
+
+    def meas_prod_times(self, simu, prod, stimulus, sample):
+        meas_start = int(self.first_duration/self.dt+1)
+        meas_stop = int(self.first_duration/self.dt+1+stimulus/self.dt)  # without flashes
+        measure = simu[meas_start:meas_stop]
+        measure = np.mean(measure, 2)
+
+        prod_lst = []
+        for i, p in enumerate(prod):
+            p_start = int(self.first_duration/self.dt+stimulus/self.dt+2)
+            p_end = int(self.first_duration/self.dt+stimulus/self.dt+2+p)
+
+            prod_lst.append(simu[p_start:p_end, :, i])
+            # print(simu[p_start:p_end,:,i].shape)
+
+        prod_sampled = [signal.resample(trial, sample) for trial in prod_lst]
+        prod_sampled = np.array(prod_sampled)
+        prod_sampled = np.mean(prod_sampled, 0)
+
+        return measure, prod_sampled
+
+    def PCA(self, stimuli_range, data=None, K=None, initI=None, experiment=True):
+        if experiment:
+            # data, has to be sliced according to stimuli and in measure and prod
+            # return meas_all prod_all
+            # meas_all
+            # prod_all
+            pass
+
+        else:
+            meas_all = []
+            prod_all = []
+            lengths_m = [0]
+            lengths_p = [0]
+            for stim in stimuli_range:
+                simulation, res, production, timeout_trials = self.simulate_parallel(stim, K, initI)
+                measure, prod_sampled = self.meas_prod_times(simulation, production, stim, int(stim/self.dt+10))
+                meas_all.extend(measure)
+                prod_all.extend(prod_sampled)
+                lengths_m.append(measure[:, :3].shape[0])
+                lengths_p.append(prod_sampled[:, :3].shape[0])
+
+        pca_m = PCA()
+        MPCA = pca_m.fit_transform(meas_all)
+        pca_p = PCA()
+        PPCA = pca_p.fit_transform(prod_all)
+
+        MPCA_lst = []
+        PPCA_lst = []
+
+        for i in range(len(stimuli_range)):
+            MPCA_lst.append(MPCA[sum(lengths_m[:i+1]):sum(lengths_m[:i+2])])
+            PPCA_lst.append(PPCA[sum(lengths_p[:i+1]):sum(lengths_p[:i+2])])
+
+        return MPCA_lst, PPCA_lst
+
+    def plot_PCA(self, stimuli_range, K, initI, colors, separate=False):
+        MPCA_lst, PPCA_lst = self.PCA(stimuli_range, data=None, K=K, initI=initI, experiment=False)
+        TOP = [3, 2, 2]
+        MIDDLE = [3, 2, 4]
+        BOTTOM = [3, 2, 6]
+
+        if separate:
+            fig = plt.figure(figsize=plt.figaspect(0.5))
+            ax = fig.add_subplot(1, 2, 1, projection='3d')
+        else:
+            fig = plt.figure(figsize=(8, 6))
+            ax = plt.axes(projection='3d')
+        for i in reversed(range(len(stimuli_range))):
+            ax.plot3D(MPCA_lst[i][:, 0],  MPCA_lst[i][:, 1],  MPCA_lst[i][:, 2], c=colors[i], alpha=0.5, marker='.')
+        ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+        ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+        ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+        plt.title('Measurement')
+
+        if separate:
+            ax = fig.add_subplot(*TOP)
+            for i, c in zip(reversed(range(len(stimuli_range))), reversed(colors)):
+                ax.plot(MPCA_lst[i][:, 0], c=c, alpha=0.5, marker='.')
+            ax = fig.add_subplot(*MIDDLE)
+            for i, c in zip(reversed(range(len(stimuli_range))), reversed(colors)):
+                ax.plot(MPCA_lst[i][:, 1], c=c, alpha=0.5, marker='.')
+            ax = fig.add_subplot(*BOTTOM)
+            for i, c in zip(reversed(range(len(stimuli_range))), reversed(colors)):
+                ax.plot(MPCA_lst[i][:, 2], c=c, alpha=0.5, marker='.')
+        plt.tight_layout()
+
+        if separate:
+            fig = plt.figure(figsize=plt.figaspect(0.5))
+            ax = fig.add_subplot(1, 2, 1, projection='3d')
+        else:
+            fig = plt.figure(figsize=(8, 6))
+            ax = plt.axes(projection='3d')
+        for i in reversed(range(len(stimuli_range))):
+            ax.plot3D(PPCA_lst[i][:, 0],  PPCA_lst[i][:, 1],  PPCA_lst[i][:, 2], c=colors[i], alpha=0.5, marker='.')
+        ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+        ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+        ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+        plt.title('Production')
+
+        if separate:
+            ax = fig.add_subplot(*TOP)
+            for i, c in zip(reversed(range(len(stimuli_range))), reversed(colors)):
+                ax.plot(PPCA_lst[i][:, 0], c=c, alpha=0.5, marker='.')
+            ax = fig.add_subplot(*MIDDLE)
+            for i, c in zip(reversed(range(len(stimuli_range))), reversed(colors)):
+                ax.plot(PPCA_lst[i][:, 1], c=c, alpha=0.5, marker='.')
+            ax = fig.add_subplot(*BOTTOM)
+            for i, c in zip(reversed(range(len(stimuli_range))), reversed(colors)):
+                ax.plot(PPCA_lst[i][:, 2], c=c, alpha=0.5, marker='.')
+        plt.tight_layout()
+
+        return MPCA_lst, PPCA_lst
