f9a351956175540e286c201aee16f856daf27f76
Katharina Bracher <katharina.bracher@mailbox.org>
2022-05-15 19:35:09 +0200

    report and figure update

---
+++ report/make-figures.sh
@@ -0,0 +1,4 @@
+#drawio -x -f pdf -o figures/defModel.drawio.pdf figures/defModel.drawio.svg
+#drawio -x -f pdf -o figures/defStages.drawio.pdf figures/defStages.drawio.svg
+drawio -x -f pdf -o figures/defCircuit.drawio.pdf figures/defCircuit.drawio.svg
+#drawio -x -f pdf -o figures/codeStructure.drawio.pdf figures/codeStructure.drawio.svg
