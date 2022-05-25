25f014883b4ee507f63fee62f10f04e058fed87f
Katharina Bracher <katharina.bracher@mailbox.org>
2022-05-25 10:23:41 +0200

    server workflow

--- code/download-results.sh
+++ code/download-results.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -rlptzv -e "ssh -p 1024" --progress --exclude=.git bracher@tunnel.bio.lmu.de:~/results ~/Documents/Studium/Master/thesis/
+rsync -rlptzv -e "ssh -p 1024" --progress --exclude=.git bracher@tunnel.bio.lmu.de:~/results ~/Documents/Studium/Master_NEURO/thesis/results
--- code/upload-to-server.sh
+++ code/upload-to-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -rlptzv -e "ssh -p 1024" --progress --delete --exclude=.git ~/Documents/Studium/Master/thesis/code/. bracher@tunnel.bio.lmu.de:~/code/
+rsync -rlptzv -e "ssh -p 1024" --progress --delete --exclude=.git ~/Documents/Studium/Master_NEURO/thesis/code/. bracher@tunnel.bio.lmu.de:~/code/
