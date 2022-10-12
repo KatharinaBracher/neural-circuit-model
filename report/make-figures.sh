cdc3fccde1d05e65e45231dfc0c9a833c51a920d
Katharina Bracher <katharina.bracher@mailbox.org>
2022-10-12 17:53:57 +0200

    thesis structure and text

--- report/make-figures.sh
+++ report/make-figures.sh
@@ -3,13 +3,16 @@
 #drawio -x -f pdf -o figures/epochs.pdf figures/epochs.drawio.svg
 #drawio -x -f pdf -o figures/trial.pdf figures/trial.drawio.svg
 #drawio -x -f pdf -o figures/interIparams.pdf figures/interIparams.drawio.svg
+drawio -x -f pdf -o figures/ranges_new.pdf figures/ranges_new.drawio.svg
+drawio -x -f pdf -o figures/highI.pdf figures/highI.drawio.svg
+
 
 #drawio -x -f pdf -o figures/supp_regimes.pdf figures/supp_regimes.drawio.svg
 #drawio -x -f pdf -o figures/supp_CV.pdf figures/supp_CV.drawio.svg
-#drawio -x -f pdf -o figures/supp_highI.pdf figures/supp_highI.drawio.svg
+drawio -x -f pdf -o figures/supp_highI.pdf figures/supp_highI.drawio.svg
 #drawio -x -f pdf -o figures/supp_experiment.pdf figures/supp_experiment.drawio.svg
 #drawio -x -f pdf -o figures/supp_experiment_high.pdf figures/supp_experiment_high.drawio.svg
-drawio -x -f pdf -o figures/supp_othererror.pdf figures/supp_othererror.drawio.svg
+#drawio -x -f pdf -o figures/supp_othererror.pdf figures/supp_othererror.drawio.svg
 drawio -x -f pdf -o figures/supp_comparison.pdf figures/supp_comparison.drawio.svg
 #drawio -x -f pdf -o figures/codeStructure.pdf figures/codeStructure.drawio.svg
 
