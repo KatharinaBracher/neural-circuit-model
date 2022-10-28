5b7e3b1b6c3a9e29d6433cff10fc1089b7251907
Katharina Bracher <katharina.bracher@mailbox.org>
2022-10-28 22:45:02 +0200

    section 5-6 adapted

--- report/make-figures.sh
+++ report/make-figures.sh
@@ -2,9 +2,9 @@
 #drawio -x -f pdf -o figures/defCircuit_nullcl.pdf figures/defCircuit_nullcl.drawio.svg
 #drawio -x -f pdf -o figures/epochs.pdf figures/epochs.drawio.svg
 #drawio -x -f pdf -o figures/trial.pdf figures/trial.drawio.svg
-drawio -x -f pdf -o figures/interIparams.pdf figures/interIparams.drawio.svg
+#drawio -x -f pdf -o figures/interIparams.pdf figures/interIparams.drawio.svg
 #drawio -x -f pdf -o figures/ranges_new.pdf figures/ranges_new.drawio.svg
-# drawio -x -f pdf -o figures/delay.pdf figures/delay.drawio.svg
+#drawio -x -f pdf -o figures/delay.pdf figures/delay.drawio.svg
 #drawio -x -f pdf -o figures/underestimation.pdf figures/underestimation.drawio.svg
 #drawio -x -f pdf -o figures/ranges_new2.pdf figures/ranges_new2.drawio.svg
 # drawio -x -f pdf -o figures/highI.pdf figures/highI.drawio.svg
@@ -19,6 +19,7 @@ drawio -x -f pdf -o figures/interIparams.pdf figures/interIparams.drawio.svg
 #drawio -x -f pdf -o figures/supp_comparison.pdf figures/supp_comparison.drawio.svg
 # drawio -x -f pdf -o figures/supp_delay.pdf figures/supp_delay.drawio.svg
 #drawio -x -f pdf -o figures/supp_ranges_new.pdf figures/supp_ranges_new.drawio.svg
+drawio -x -f pdf -o figures/supp_underestimationE.pdf figures/supp_underestimationE.drawio.svg
 #drawio -x -f pdf -o figures/codeStructure.pdf figures/codeStructure.drawio.svg
 
 #drawio -x -f pdf -o poster/poster.pdf poster/poster.drawio.svg
